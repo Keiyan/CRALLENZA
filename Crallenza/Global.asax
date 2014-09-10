6813a59a6ea29a6eda526b0dccd7bade36bfb387
Djodjes <damien.georges@gmail.com>
2014-09-10 11:27:15 +0200

    Initial push for crallenza

---
+++ Crallenza/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crallenza.MvcApplication" Language="C#" %>
---
+++ Crallenza/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crallenza.Startup))]
+namespace Crallenza
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
