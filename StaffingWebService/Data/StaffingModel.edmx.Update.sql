014e11b96e166756cfb9b4eec343d6f2255e9402
piafpamphile <piafpamphile@gmail.com>
2014-09-10 15:29:48 +0200

    Update db and config

--- StaffingWebService/Data/StaffingModel.edmx.Update.sql
+++ StaffingWebService/Data/StaffingModel.edmx.Update.sql
@@ -4,22 +4,12 @@ GO
 
 DELETE FROM [dbo].[ConsultantTable] 
 
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Alexandre','Démoulin','alexandre.demoulin@cellenza.com','alexandre.demoulin@cellenza.com')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Alexandre','Plassais','alexandre.plassais@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Anne','Pedro','anne.pedro@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Arnaud','Hego','arnaud.hego@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Arnaud','Villenave','arnaud.villenave@cellenza.com','avillenave@hotmail.com')
+INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Jonathan','Pamphile','jonathan.pamphile@cellenza.com','')
 INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Boris','Dhambahadour','boris.dhambahadour@cellenza.com','')
 INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','David','Tran','david.tran@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Eric','Galiano','eric.galiano@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Fathi','Bellahcene','fathi.bellahcene@cellenza.com','fbellahc@hotmail.com')
 INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Jason','De Oliveira','jason.deoliveira@cellenza.com','jason.oliveira@live.com')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Jonathan','Pamphile','jonathan.pamphile@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Kevin','Eliçagoyen','kevin.elicagoyen@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Michel','Perfetti','michel.perfetti@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Nicholas','Suter','nicholas.suter@cellenza.com','nicholas.suter@cellenza.com')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Pierre-Henri','Gache','pierre-henri.gache@cellenza.com','pierrehenri.gache@outlook.com')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Romain','Lassalle','romain.lassalle@cellenza.com','')
-INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,0,'20130501','Simon','Gilquin','simon.gilquin@cellenza.com','')
+INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Georges','Damien','georges.damien@cellenza.com','')
+INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Mick','Philippon','mick.philippon@cellenza.com','')
+INSERT INTO [dbo].[ConsultantTable] (Actif,Admin,DateArrivee,Nom,Prenom,Email,LiveEmail) VALUES (1,1,'20130501','Olivier','Delmotte','olivier.delmotte@cellenza.com','')
 
 SELECT * FROM [dbo].[ConsultantTable] 
